4843221c3159f450e0a980ce3c59983280571d8b
Lily Cheng <lilychxng@gmail.com>
2022-02-07 13:28:14 -0500

    classwork

---
+++ Assets/Week 3/Scripts/moveCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour
+{
+    public Transform mover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    Camera.main.position = Vector3(0.0f, mover.position.y, 0.0f);
+
+    }
+}
