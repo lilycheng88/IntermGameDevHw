ae2453879d7afcdc1edecc1a02fe725bac9aadd9
Lily Cheng <lilychxng@gmail.com>
2022-02-13 21:16:33 -0500

    Added scenes and trigger collisions

--- Assets/Week 3/Scripts/checkCollision.cs
+++ Assets/Week 3/Scripts/checkCollision.cs
@@ -5,19 +5,55 @@ using UnityEngine;
 public class checkCollision : MonoBehaviour
 {
     //CHANGES CAMERA POSITION
+    //BoxCollider2D boxCollider;
+    //public GameObject obj;
+    //public string objName;
+
+    //private void Awake()
+    //{
+    //    boxCollider = GetComponent<BoxCollider2D>();
+    //}
+
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    objName = obj.name;
+    //    moveCamera.ChangePlayer(objName);
+    //}
+
     BoxCollider2D boxCollider;
+    SpriteRenderer spriteRenderer;
+    //public Transform mover;
+    //public Transform targetLocation;
     public GameObject obj;
     public string objName;
+    public float cameraSize;
 
     private void Awake()
     {
         boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    // Start is called before the first frame update
+    void Start()
     {
+        boxCollider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //spriteRenderer.color = new Color(1.0f, 0.5f, 0.25f, 0.5f); //red, blue, green, alpha
+        //// mover.position = new Vector3(5.0f, 0.0f, 0.0f); // x, y, z
+        //mover.position = targetLocation.position;
         objName = obj.name;
         moveCamera.ChangePlayer(objName);
+        moveCamera.SetSize(cameraSize);
+        Debug.Log("Entered Trigger");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Exit Trigger.");
     }
 
 }
--- Assets/Week 3/Scripts/moveCamera.cs
+++ Assets/Week 3/Scripts/moveCamera.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class moveCamera : MonoBehaviour
 {
     public static GameObject player;
+    public static Camera myCamera; 
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Launched Ball");
+        myCamera = GetComponent<Camera>();
     }
 
     public static void ChangePlayer(string name)
     {
         player = GameObject.Find(name);
+    }
+
+    public static void SetSize(float value)
+    {
+        myCamera.orthographicSize = value;
     }
 
     // Update is called once per frame
